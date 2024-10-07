36cf34b9320f827f0410ae831b34170331cea41d
choetaehyeong <taehyoung0528@gmail.com>
2024-10-07 17:03:58 +0900

    Refactor: Shoping-mall/App

--- shopping-mall/src/App.jsx
+++ shopping-mall/src/App.jsx
@@ -1,17 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import React, { useState } from "react";
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import './App.css';
-import data from './data';
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "./App.css";
+import data from "./data";
 
 function App() {
+  let [tshirt, setTshirt] = useState(data);
 
-  let [tshirt, setTshirt] = useState(data)
-
-  return(
+  return (
     <div className="App">
-
-       <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">KingSmile Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -25,7 +23,9 @@ function App() {
                 <NavDropdown.Item href="#action/3.2">
                   Another action
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">찾아오는길</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  찾아오는길
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
@@ -34,42 +34,40 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Container>
-    </Navbar>
+      </Navbar>
 
-    <div className="jumbotron background">
+      <div className="jumbotron background">
+        <h1 className="display-4">Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
 
-      <h1 className="display-4">Hello, world!</h1>
-      <p >This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </div>
-
-    <div className='container'>
-      <div className='row'>
-          <div className='col-md-4'>
-            <img src='https://github.com/ai-edu-pro/busan/blob/main/t1.jpg?raw=truehttps://thumb.cjonstyle.net/unsafe/fit-in/236x236/itemimage.cjonstyle.net/goods_images/20/089/2035875089L.jpg?timestamp=20240722180523' />
-            <h4>{ tshirt[0].title }</h4>
-            <p>{tshirt[0].content} </p>
-            <p>{tshirt[0].price}</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://github.com/ai-edu-pro/busan/blob/main/t2.jpg?raw=truehttps://thumb.cjonstyle.net/unsafe/fit-in/236x236/itemimage.cjonstyle.net/goods_images/20/089/2035875089L.jpg?timestamp=20240722180523' />
-            <h4>{ tshirt[1].title }</h4>
-            <p>{tshirt[1].content} </p>
-            <p>{tshirt[1].price}</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://github.com/ai-edu-pro/busan/blob/main/t5.jpg?raw=truehttps://thumb.cjonstyle.net/unsafe/fit-in/236x236/itemimage.cjonstyle.net/goods_images/20/089/2035875089L.jpg?timestamp=20240722180523' />
-            <h4>{ tshirt[2].title }</h4>
-            <p>{tshirt[2].content} </p>
-            <p>{tshirt[2].price}</p>
-          </div>
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
+        </a>
+      </div>
+      <div className="container">
+        <div className="row">
+          <Card />
+        </div>
       </div>
     </div>
-
+  );
+}
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src="https://github.com/ai-edu-pro/busan/blob/main/t1.jpg?raw=truehttps://thumb.cjonstyle.net/unsafe/fit-in/236x236/itemimage.cjonstyle.net/goods_images/20/089/2035875089L.jpg?timestamp=20240722180523" />
+      <h4>{props.title}</h4>
+      <p>{props.content} </p>
+      <p>{props.price}</p>
     </div>
-  )
+  );
 }
 
 export default App;
